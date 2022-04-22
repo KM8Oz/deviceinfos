6618f84580b76c434712902cd5dfa7526166590b
km8oz <ouazmir@gmail.com>
2022-04-22 22:25:18 +0300

    0.0.3

--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,38 @@
 #[macro_use]
 extern crate napi_derive;
 extern crate whoami;
+
 #[napi]
 fn realname() -> String { 
   return whoami::realname();
 }
+
 #[napi]
 fn username() -> String { 
   return whoami::username();
 }
+
 #[napi]
 fn lang() -> String { 
-  let s: String = whoami::lang().collect();
+  let s: String = whoami::lang().collect::<Vec<String>>().join(", ");
   return s;
 }
+
 #[napi]
 fn devicename() -> String { 
   return whoami::devicename();
 }
+
 #[napi]
 fn hostname() -> String { 
   return whoami::hostname();
 }
+
 #[napi]
 fn distro() -> String { 
   return whoami::distro();
 }
+
 #[napi]
 fn desktop_env() -> String { 
   return whoami::desktop_env().to_string();
