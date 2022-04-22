bc359c5c456ebe2f5b464aa5a919c2f021a6d09e
km8oz <ouazmir@gmail.com>
2022-04-22 17:34:28 +0300

    @km8oz fix CI

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -4,16 +4,19 @@ import { realname, username, lang, devicename, hostname, distro, desktopEnv } fr
 
 test('realname from native', (t) => {
   if(realname()){
+    console.log(realname());
     t.pass()
   }
 })
 test('username from native', (t) => {
   if(username()){
+    console.log(username());
     t.pass()
   }
 })
 test('lang from native', (t) => {
   if(lang()){
+    console.log(lang());
     t.pass()
   }
 })
